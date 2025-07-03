f69ae1c0322ec4afaf7a8bfc0ba4865d9c988424
vicfontdev <fcovcfn@gmail.com>
2025-07-03 15:18:14 -0300

    rotas para usuários e tarefas

--- index.js
+++ index.js
@@ -33,13 +33,47 @@ app.get("/boas-vindas/:nome", (request, response) => {
     response.send(`Seja bem-vindo, ${request.params.nome}`);
 });
 
+//buscar usuarios
+
+app.get("/usuarios", async (req, res) => {
+    res.send(await executarSQL("SELECT * FROM usuarios;"))
+})
+
 //cria o usuario
 app.post("/usuarios", async (req, res) => {
     const request = await executarSQL(`INSERT INTO usuarios (nome, email, senha) VALUES ('${req.body.nome}', '${req.body.email}', '${req.body.senha}')`)
     if(request.affectedRows > 0){
         res.send("Usuário cadastrado com sucesso!")
-    }else
+    }else{
         res.send("Ocorreu um erro.")
+    }
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+    const request = await executarSQL(`DELETE FROM usuarios where id = ${req.params.id}`)
+    if (request.affectedRows > 0){
+        res.send("Usuário deletado com sucesso!")
+    }else{
+        res.send("Ocorreu um erro.")
+    }
+
+})
+
+app.get("/tarefas", async (req, res) => {
+    res.send(await executarSQL("SELECT * FROM tarefas;"))
+})
+
+app.post("/tarefas", async (req, res) => {
+    try {
+        const request = await executarSQL(`INSERT INTO tarefas (titulo, descricao, usuario_id) VALUES ('${req.body.titulo}', '${req.body.descricao}', '${req.body.usuario_id}')`)
+        if(request.affectedRows > 0){
+            res.send("Tarefa cadastrada com sucesso!")
+        }else{
+            res.send("Ocorreu um erro.")
+        }
+    } catch (error) {
+        res.send(error.message);
+    }
 });
 
 app.listen(port, () => {

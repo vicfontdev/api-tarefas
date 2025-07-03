e9dca0f9ed69d4221a6669fb83d174eea5035c85
vicfontdev <fcovcfn@gmail.com>
2025-07-03 14:19:16 -0300

    api do projeto tarefas

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mysql from "mysql2/promise";
+
+const app = express();
+const port = 8000;
+
+async function executarSQL(comandoSQL) {
+    const conexao = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "1234",
+        port: "3307",
+        database: "tarefas_db"
+    })
+
+    const [result] = await conexao.query(comandoSQL);
+    conexao.end();
+
+    return result;
+}
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.send("Olá, mundo");
+})
+
+app.get("/boas-vindas", (request, response) => {
+    response.send(`Seja bem-vindo`);
+})
+
+app.get("/boas-vindas/:nome", (request, response) => {
+    response.send(`Seja bem-vindo, ${request.params.nome}`);
+});
+
+//cria o usuario
+app.post("/usuarios", async (req, res) => {
+    const request = await executarSQL(`INSERT INTO usuarios (nome, email, senha) VALUES ('${req.body.nome}', '${req.body.email}', '${req.body.senha}')`)
+    if(request.affectedRows > 0){
+        res.send("Usuário cadastrado com sucesso!")
+    }else
+        res.send("Ocorreu um erro.")
+});
+
+app.listen(port, () => {
+    console.log(`Servidor de pé: http://localhost:${port}`);
+});
